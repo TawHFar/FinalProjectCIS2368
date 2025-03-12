db37aa5d18ebecfc29838dcf0ba18ed28ae3898e
VanessaTechPro <jackitasoriano@icloud.com>
2025-03-12 16:03:53 -0500

    borrow

--- testing.py
+++ testing.py
@@ -199,7 +199,7 @@ def borrow_book():
         return "Book unavailable."
     
     #Checks if customer is in borrowing records and confirms if they are able to borrow book
-    borrow_query = 'SELECT * FROM borrowingrecords WHERE book_id=%s AND customerid=%s AND returndate is NULL'
+    borrow_query = 'SELECT * FROM borrowingrecords WHERE customerid=%s AND returndate is NULL'
     cust_value = (book_id,cust_id)
     check_cust = execute_read_query(conn, borrow_query, cust_value)
 
